5e0bc85a296198413b6eae06865e20b0f15772e3
Dalia Camacho García Formentí <daliaf172@gmail.com>
2019-07-31 10:07:11 -0400

    Make some of the steps optional

--- heatmap/heat_map.py
+++ heatmap/heat_map.py
@@ -1,24 +1,34 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-from skimage import transform
+from cv2 import resize
 
-def add(image, heat_map, alpha=0.6, display=False, save=None, cmap='viridis', axis='on', verbose=False):
+def add(image, heat_map, alpha=0.6, display=False, save=None, cmap='viridis', axis='off', 
+        verbose=False, normalise=False, my_resize=False, colormap=False, dpi=4):
 
     height = image.shape[0]
     width = image.shape[1]
 
     # resize heat map
-    heat_map_resized = transform.resize(heat_map, (height, width))
-
+    if my_resize:
+        heat_map_resized = resize(heat_map, (height, width))
+    else:
+        heat_map_resized=heat_map
     # normalize heat map
     max_value = np.max(heat_map_resized)
     min_value = np.min(heat_map_resized)
-    normalized_heat_map = (heat_map_resized - min_value) / (max_value - min_value)
-
+    if normalise:
+        normalised_heat_map = (heat_map_resized - min_value) / (max_value - min_value)*255 
+    else:
+        normalised_heat_map= heat_map_resized
     # display
+    fig=plt.figure(figsize=(height/dpi,width/dpi), dpi=dpi)
     plt.imshow(image)
-    plt.imshow(255 * normalized_heat_map, alpha=alpha, cmap=cmap)
+    if colormap:
+        plt.imshow(normalised_heat_map, alpha=alpha, cmap=cmap)
+    else:
+        plt.imshow(normalised_heat_map, alpha=alpha)
+        
     plt.axis(axis)
 
     if display:
